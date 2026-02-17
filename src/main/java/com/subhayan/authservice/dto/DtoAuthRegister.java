49725a06577f8348bb45411bce2edd2b22ee5f5e
Subhayan0022 <msubhayan529@gmail.com>
2026-02-17 20:37:44 +0530

    Add new migration fields - Expand DTO and Entity - Salutation Enum

--- src/main/java/com/subhayan/authservice/dto/DtoAuthRegister.java
+++ src/main/java/com/subhayan/authservice/dto/DtoAuthRegister.java
@@ -1,17 +1,34 @@
 package com.subhayan.authservice.dto;
 
 import com.subhayan.authservice.entity.Role;
+import com.subhayan.authservice.entity.Salutation;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class DtoAuthRegister {
     public record UserRegisterRequestDTO(
+            @NotBlank Salutation salutation,
+            @NotBlank String firstName,
+            @NotBlank String lastName,
             @NotBlank @Email String email,
-            @NotBlank @Size(min = 8) String password
+            @NotBlank @Size(min = 8) String password,
+            @NotBlank String phoneNumber,
+            @NotBlank LocalDate dateOfBirth
+
     ) {}
 
-    public record UserRegisterResponseDTO(UUID id, String email, Role role) {}
+    public record UserRegisterResponseDTO(
+            UUID id,
+            Salutation salutation,
+            String firstName,
+            String lastName,
+            String email,
+            String phoneNumber,
+            LocalDate dateOfBirth,
+            Role role
+    ) {}
 }
