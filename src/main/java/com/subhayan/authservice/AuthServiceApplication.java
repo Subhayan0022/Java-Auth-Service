3d7a91b42afa7641a4dec9962af2e2d808bb4e25
Subhayan0022 <msubhayan529@gmail.com>
2026-02-21 18:32:27 +0530

    configure Redis and initialize it at startup

--- src/main/java/com/subhayan/authservice/AuthServiceApplication.java
+++ src/main/java/com/subhayan/authservice/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.subhayan.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // Enables caching infra at startup
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
