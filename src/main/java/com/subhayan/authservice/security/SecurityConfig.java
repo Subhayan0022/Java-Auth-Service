710b7c4cb7394593a240ad6c4c9449fd0369fad3
Subhayan0022 <msubhayan529@gmail.com>
2026-02-21 19:20:53 +0530

    add actuator endpoint to whitelist

--- src/main/java/com/subhayan/authservice/security/SecurityConfig.java
+++ src/main/java/com/subhayan/authservice/security/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/auth/**",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/actuator/**"
     };
 
     @Bean
